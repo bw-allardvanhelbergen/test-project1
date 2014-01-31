a25733a2fe196fe1950be80219c0a9d003a8756f
Allard van Helbergen <allard@brandwatch.com>
2014-01-31 02:21:48 +0100

    .

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task.
-  grunt.registerTask('default', [less]);
-
+  grunt.registerTask('default', ['less']);
 };
