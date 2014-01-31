c9ec87846b5b440c9d4f69dc40cce902e17932e2
Allard van Helbergen <allard@brandwatch.com>
2014-01-31 02:15:00 +0100

    Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    less:{
+      compile:{
+        files: {
+          'css/app.css': 'less/app.less'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task.
+  grunt.registerTask('default', [less]);
+
+};
